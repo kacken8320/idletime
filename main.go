1c6bdacd8b441fb3c8529610b773cd732757d2f0
kacko <emailokacko>
2025-07-29 22:29:25 +0200

    docker postgres & docker-compose up

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	_ "github.com/jackc/pgx/v5"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	fmt.Println("sars");
+	connStr := "postgres://user:password@localhost/dbname?sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("connected to db");
 }
